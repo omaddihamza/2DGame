d739ec0a1b4c863c2aa85ea6d94e185d6fa69005
hamza omaddi <hamza.omaddi@gmail.com>
2024-12-28 20:00:27 +0100

    Draw an object on the screen with Graphics2D

---
+++ src/main/java/org/sid/GamePanel.java
@@ -0,0 +1,76 @@
+package org.sid;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    // SCREEN SETTING
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale; //48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    final int screenHeight = tileSize * maxScreenRow; //768 pixels
+
+    //FPS
+    int FPS = 60;
+
+    KeyHandler keyHandler = new KeyHandler();
+    Thread gameThread;
+
+    //set player"s default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        // if set to true, all the drawing from this component will be done in an offscreen painting buffer
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        //with this, this gamepanel can be focused to receive key input
+        this.setFocusable(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+      while (gameThread !=null){
+          //returns the current value of the running java virtual machine's high-resolution time source in nanoseconds
+          long currentTime = System.nanoTime();
+          //update information such as character position
+          update();
+          //draw the screen with the update information
+          repaint();
+      }
+    }
+
+    public void update(){
+
+    if (keyHandler.upPressed == true){
+        playerY -= playerSpeed;
+    }else if (keyHandler.downPressed == true){
+        playerY += playerSpeed;
+    }else if (keyHandler.leftPressed == true){
+        playerX -= playerSpeed;
+    }else if (keyHandler.rightPressed == true){
+        playerX += playerSpeed;
+    }
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.white);
+        g2d.fillRect(playerX, playerY, tileSize, tileSize);
+        g2d.dispose();
+    }
+
+}
--- src/main/java/org/sid/Main.java
+++ src/main/java/org/sid/Main.java
@@ -1,9 +1,23 @@
 package org.sid;
 
-
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Create a window
+        JFrame window = new JFrame();
+        //this tel the window properly close when user clicks the x button
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
 
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        // Make the window visible
+        window.setVisible(true);
+        gamePanel.startGameThread();
     }
 }
